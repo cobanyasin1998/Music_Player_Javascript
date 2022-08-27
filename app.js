92adb6adfc890c781a6c8993c7350cc52cdd1f51
Yasin Çoban <yasincoban1998@outlook.com>
2022-08-27 21:00:44 +0300

    controls added

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
-
 const singer = document.querySelector("#music-details .singer");
+
 const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
 const next = document.querySelector("#controls #next");
 
 const audio = document.querySelector("#audio-play");
-
 
 const player = new MusicPlayer(musicList);
 
@@ -24,6 +23,41 @@ function displayMusic(music) {
   audio.src = "mp3/" + music.file;
 }
 
-play.addEventListener("click",()=>{
-    audio.play();
+play.addEventListener("click", () => {
+  const isMusicPlayer = container.classList.contains("playing");
+  isMusicPlayer ? pauseMusic() : playMusic();
 });
+
+prev.addEventListener("click", () => {
+  prevMusic();
+});
+next.addEventListener("click", () => {
+  nextMusic();
+});
+function pauseMusic() {
+  audio.pause();
+  container.classList.remove("playing");
+  play.classList = "fa-solid fa-play";
+}
+function playMusic() {
+  audio.play();
+  container.classList.add("playing");
+  play.classList = "fa-solid fa-pause";
+}
+function prevMusic() {
+  debugger;
+  player.previous();
+
+  let music = player.getMusic();
+  displayMusic(music);
+
+  playMusic();
+}
+function nextMusic() {
+  player.next();
+
+  let music = player.getMusic();
+  displayMusic(music);
+
+  playMusic();
+}
