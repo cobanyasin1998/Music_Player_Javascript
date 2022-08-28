b139d389a18bd49ad442fbd40ac34c387fa7dc97
Yasin Çoban <yasincoban1998@outlook.com>
2022-08-28 13:19:07 +0300

    sound controls

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
 const singer = document.querySelector("#music-details .singer");
-
 const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
 const next = document.querySelector("#controls #next");
-
 const audio = document.querySelector("#audio-play");
-
 const duration = document.querySelector("#duration");
 const currentTime = document.querySelector("#current-time");
-
 const progressBar = document.querySelector("#progress-bar");
+
+const volume = document.querySelector("#volume");
+const volumeBar = document.querySelector("#volume-bar");
 
 const player = new MusicPlayer(musicList);
 
@@ -85,4 +84,37 @@ audio.addEventListener("timeupdate", () => {
 progressBar.addEventListener("input", () => {
   currentTime.textContent = calculateTime(progressBar.value);
   audio.currentTime = progressBar.value;
+});
+
+let sesDurumu = "sesli";
+
+volumeBar.addEventListener("click", (e) => {
+  const value = e.target.value;
+  audio.volume = value/100;
+  if(value == 0){
+    audio.muted = true;
+    sesDurumu = "sessiz";
+    volume.classList = "fa-solid fa-volume-xmark";
+    volumeBar.value = 0;
+  }
+  else{
+    audio.muted = false;
+    sesDurumu = "sesli";
+    volume.classList = "fa-solid fa-volume-high";
+    volumeBar.value = 100;
+  }
+});
+
+volume.addEventListener("click", () => {
+  if (sesDurumu === "sesli") {
+    audio.muted = true;
+    sesDurumu = "sessiz";
+    volume.classList = "fa-solid fa-volume-xmark";
+    volumeBar.value = 0;
+  } else {
+    audio.muted = false;
+    sesDurumu = "sesli";
+    volume.classList = "fa-solid fa-volume-high";
+    volumeBar.value = 100;
+  }
 });
